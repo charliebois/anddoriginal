ca8b7f90e0f6eb767fd270988c673be8e4445f59
Kcharle <ky.charle@gmail.com>
2016-06-19 17:45:12 -0400

    variable product view and styling

---
+++ wp-content/themes/aand/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Single variation cart button
+ *
+ * @see 	http://docs.woothemes.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.5.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+global $product;
+?>
+<div class="woocommerce-variation-add-to-cart variations_button">
+	
+	<button type="submit" class="single_add_to_cart_button button alt">Buy</button>
+	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->id ); ?>" />
+	<input type="hidden" name="product_id" value="<?php echo absint( $product->id ); ?>" />
+	<input type="hidden" name="variation_id" class="variation_id" value="0" />
+</div>
