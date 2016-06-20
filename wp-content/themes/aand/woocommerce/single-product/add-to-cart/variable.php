ee5094a618887e63634379dfb28af3117e0b5cae
charliebois <kyle@charliebois.com>
2016-06-20 19:35:21 -0400

    updated styling to product description

--- wp-content/themes/aand/woocommerce/single-product/add-to-cart/variable.php
+++ wp-content/themes/aand/woocommerce/single-product/add-to-cart/variable.php
@@ -24,35 +24,36 @@ $attribute_keys = array_keys( $attributes );
 
 do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-<a href="#" class="button">Sizing Chart</a>
+<a href="#" class="sizing-chart">Sizing Chart</a>
 
 <form class="variations_form cart" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->id ); ?>" data-product_variations="<?php echo htmlspecialchars( json_encode( $available_variations ) ) ?>">
 	<?php do_action( 'woocommerce_before_variations_form' ); ?>
 
 	<?php if ( ! $product->is_sold_individually() ) : ?>
+		<div class="label">
+			<label class="quantity" for="quantity">Quantity</label>
+		</div>
 		<?php woocommerce_quantity_input( array( 'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 ) ); ?>
 	<?php endif; ?>
 
 	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
 		<p class="stock out-of-stock"><?php _e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
 	<?php else : ?>
-		<table class="variations" cellspacing="0">
-			<tbody>
-				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
 
-					<tr>
-						<td class="label"><label for="<?php echo sanitize_title( $attribute_name ); ?>"><?php echo wc_attribute_label( $attribute_name ); ?></label></td>
-						<td class="value">
+		<div class="variations">
+				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
+						<div class="label">
+							<label for="<?php echo sanitize_title( $attribute_name ); ?>"><?php echo wc_attribute_label( $attribute_name ); ?></label>
+						</div>
+						<div class="value">
 							<?php
 								$selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ? wc_clean( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) : $product->get_variation_default_attribute( $attribute_name );
 								wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected ) );
 								echo end( $attribute_keys ) === $attribute_name ? apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . __( 'Clear', 'woocommerce' ) . '</a>' ) : '';
 							?>
-						</td>
-					</tr>
+						</div>
 		        <?php endforeach;?>
-			</tbody>
-		</table>
+		</div>
 
 		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
